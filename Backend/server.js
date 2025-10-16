455375c99382f4efe6a79fb034d4b10db4ce6932
code23-13 <code23-13@code23-13s-iMac.local>
2025-10-16 20:56:35 +0300

    yeni güncellemeler yapılmaya devam ediyor

--- Backend/server.js
+++ Backend/server.js
@@ -79,7 +79,7 @@ app.use((req, res) => {
     });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 const server = app.listen(PORT, () => {
     console.log(`🚀 Server ${PORT} portunda çalışıyor`);
